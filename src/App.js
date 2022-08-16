9840e415eaf22b095be324c9e1aa7e138884d416
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-08-15 18:17:54 -0700

    dependencies, scaffolding, testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import RssItem from './Components/RssItem';
+
+import { useState } from 'react';
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from '@mantine/core';
 
 function App() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppShell
+        styles={{
+          main: {
+            background:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        }}
+        navbarOffsetBreakpoint='sm'
+        asideOffsetBreakpoint='sm'
+        navbar={
+          <Navbar
+            p='md'
+            hiddenBreakpoint='sm'
+            hidden={!opened}
+            width={{ sm: 200, lg: 300 }}>
+            <Text>My Views</Text>
+          </Navbar>
+        }
+        aside={
+          <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+            <Aside p='md' hiddenBreakpoint='sm' width={{ sm: 200, lg: 300 }}>
+              <Text>Application sidebar</Text>
+            </Aside>
+          </MediaQuery>
+        }
+        footer={
+          <Footer height={60} p='md'>
+            Application footer
+          </Footer>
+        }
+        header={
+          <Header height={70} p='md'>
+            <div
+              style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+              <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size='sm'
+                  color={theme.colors.gray[6]}
+                  mr='xl'
+                />
+              </MediaQuery>
+
+              <Text>RSS Reader</Text>
+            </div>
+          </Header>
+        }>
+        {/* APP COMNPONENTS GO HERE */}
+        <Text>OUR APP</Text>
+        <RssItem />
+      </AppShell>
+    </>
   );
 }
 
