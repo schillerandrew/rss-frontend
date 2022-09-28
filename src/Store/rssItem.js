10e8d298d1d276a1bc768348f4ec0cd6b2929046
schillerandrew <schiller.andrew@gmail.com>
2022-09-28 12:43:40 -0700

    changed Axios URL from localhost to deployed backend

--- src/Store/rssItem.js
+++ src/Store/rssItem.js
@@ -13,7 +13,7 @@ export const setRssItem = createAction(SET_RSSITEM);
 export const getRssItems = (rssFeedUrl) => async (dispatch, getState) => {
   if (rssFeedUrl) {
     // console.log('event.rssFeedUrl', rssFeedUrl);
-    let res = await axios.get(`http://localhost:3001/feeds?url=${rssFeedUrl}`);
+    let res = await axios.get(`https://rss-reader.herokuapp.com/feeds?url=${rssFeedUrl}`);
     if (res.data) {
       // console.log('RES.DATA', res.data);
       dispatch(setRssItem(res.data));
